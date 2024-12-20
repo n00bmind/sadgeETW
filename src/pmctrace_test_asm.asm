8172751d9ca8254f938b8d7f922a4260b6e8f0b6
n00bmind <n00bmindr0b0t@gmail.com>
2024-12-20 18:58:17 +0000

    + Skeleton of a solution allowing multiple levels of zones in multiple threads

--- src/pmctrace_test_asm.asm
+++ src/pmctrace_test_asm.asm
@@ -19,12 +19,9 @@ CountNonZeroesWithBranch:
     xor r10, r10
 
 .loop:
-    ; FIXME This destroys the rolling count of non-zeroes in rax, but for some extremely
-    ; weird reason I cannot get the same results with Casey's test program if I use bl here!
-    ; (even after pushing / popping rbx to the stack)
-    mov al, [rdx + r10]
+    mov r11b, [rdx + r10]
     
-    cmp al, 0
+    cmp r11b, 0
     jz .skipsum
     inc rax
 .skipsum:
